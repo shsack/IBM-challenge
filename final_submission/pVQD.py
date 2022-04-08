2cbc931c2cf273677ef687545f311390406f470b
StefanoBarison <stefano.barison@epfl.ch>
2022-04-08 22:43:22 +0200

    Substantial improvements of the final notebook

--- final_submission/pVQD.py
+++ final_submission/pVQD.py
@@ -1,29 +1,19 @@
 import numpy as np 
-import json
-import functools
-import itertools
-import matplotlib.pyplot as plt 
-from scipy   import  linalg as LA 
-
-
-import qiskit
-from qiskit                               import Aer, execute
-from qiskit.quantum_info       import Pauli
-from qiskit.utils                         import QuantumInstance
-from qiskit.opflow             import PauliOp, SummedOp, CircuitSampler, StateFn
-from qiskit.circuit                       import ParameterVector
-from qiskit.opflow.evolutions             import Trotter, PauliTrotterEvolution
-
-from qiskit.opflow.state_fns      import CircuitStateFn
-from qiskit.opflow.expectations   import PauliExpectation, AerPauliExpectation
-from qiskit.opflow.primitive_ops  import CircuitOp
 from qiskit.opflow                import Z, I
 
 
 # Useful functions
 
 def projector_zero(n_qubits):
-    # This function create the global projector |00...0><00...0|
+
+    '''
+    This function create the global projector |00...0><00...0|
+    Args:
+        - n_qubits: the number of qubits of the system
+    Returns:
+        The projector |00...0><00...0| as a Qiskit Opflow operator
+
+    '''
     from qiskit.opflow            import Z, I
 
     prj_list = [0.5*(I+Z) for i in range(n_qubits)]
@@ -34,118 +24,65 @@ def projector_zero(n_qubits):
 
     return prj
 
-def projector_zero_local(n_qubits):
-    # This function creates the local version of the cost function 
-    # proposed by Cerezo et al: https://www.nature.com/articles/s41467-021-21728-w
-    from qiskit.opflow          import Z, I
-
-    tot_prj = 0
-
-    for k in range(n_qubits):
-        prj_list = [I for i in range(n_qubits)]
-        prj_list[k] = 0.5*(I+Z)
-        prj = prj_list[0]
-
-        for a in range(1,len(prj_list)):
-            prj = prj^prj_list[a]
-
-        tot_prj += prj
-
-    tot_prj = (1/n_qubits)*tot_prj
-    
-    return tot_prj
-
 
 def ei(i,n):
+
+    '''
+    This function returns the i-th basis vector on a vector space of dimension n.
+    This is useful to implement the parameter-shift rule.
+
+    Args:
+        - i: the 
+        - n: the dimension of the vector
+
+    Returns:
+        An array of dimension n with all 0 except for the i-th component, set to 1
+
+    '''
     vi = np.zeros(n)
     vi[i] = 1.0
     return vi[:]
 
 
-def construct_total_circuit(U_dt,time_step,ansatz,params_vec,left,right):
-    ## This function creates the circuit that will be used to evaluate overlap and its gradient
+def adam_gradient(params,count,m,v,g):
 
-    l_circ  = ansatz.assign_parameters({params_vec: left})
-    r_circ  = ansatz.assign_parameters({params_vec: right})
-    zero_prj = StateFn(projector_zero(U_dt.num_qubits),is_measurement = True)
-    state_wfn = zero_prj @ StateFn(r_circ.combine(U_dt)+ l_circ.inverse())
+    '''
+    This function implements ADAM optimizer from scratch.
+    Given an array of parameters and the gradient it returns a new array of parameters,
+    following arXiv: 1412.6980 .
+    Args:
+        - params: array of parameters to optimize
+        - count : the optimization step, counting from 1
+        - m     : the moving averages of the gradient, updated after every step
+        - v     : the moving averages of the squared gradient, updated after each step
+        - g     : the gradient 
+        
+    Returns:
+        The updated array of parameter 
 
-    return state_wfn
+    '''
 
+    # Set the ADAM hyperparameters 
+    # β1,β2 control he exponential decay rates of the moving averages
+    β1 = 0.9
+    β2 = 0.999
+    # Regularization constant
+    ε  = 1e-8
+    # Parameter wise adaptive learning rate
+    α  = [0.001 for i in range(len(params))]
+    if count == 0:
+        count = 1
 
-def compute_overlap_and_gradient(left,right,state_wfn,parameters,shift,expectator,sampler):
+    new_params = [0 for i in range(len(params))]
 
-	nparameters = len(parameters)
-	# build dictionary of parameters to values
-	# {left[0]: parameters[0], .. ., right[0]: parameters[0] + shift[0], ...}
-	# First create the dictionary for overlap
-	values_dict = [dict(zip(right[:] + left[:], parameters.tolist() + (parameters + shift).tolist()))]
-		
+    for i in range(len(params)):
+        m[i] = β1 * m[i] + (1 - β1) * g[i]
+        v[i] = β2 * v[i] + (1 - β2) * np.power(g[i],2)
 
-	# Then the values for the gradient
-	for i in range(nparameters):
-		values_dict.append(dict(zip(right[:] + left[:], parameters.tolist() + (parameters + shift + ei(i,nparameters)*np.pi/2.0).tolist())))
-		values_dict.append(dict(zip(right[:] + left[:], parameters.tolist() + (parameters + shift - ei(i,nparameters)*np.pi/2.0).tolist())))
+        α[i] = α[i] * np.sqrt(1 - np.power(β2,count)) / (1 - np.power(β1,count))
 
-	# Now evaluate the circuits with the parameters assigned
+        new_params[i] = params[i] + α[i]*(m[i]/(np.sqrt(v[i])+ε))
 
-	results = []
-
-	for values in values_dict:
-		sampled_op = sampler.convert(state_wfn,params=values)
-
-		mean  = sampled_op.eval().real
-		est_err = 0
-
-		results.append([mean,est_err])
-
-	E = np.zeros(2)
-	g = np.zeros((nparameters,2))
-
-	E[0],E[1] = results[0]
-
-	for i in range(nparameters):
-		rplus  = results[1+2*i]
-		rminus = results[2+2*i]
-		# G      = (Ep - Em)/2
-		# var(G) = var(Ep) * (dG/dEp)**2 + var(Em) * (dG/dEm)**2
-		g[i,:] = (rplus[0]-rminus[0])/2.0,np.sqrt(rplus[1]**2+rminus[1]**2)/2.0
-
-	return E,g 
-
-def adam_gradient(parameters,shift,count,m,v,g):
-	## This function implements adam optimizer
-	beta1 = 0.9
-	beta2 = 0.999
-	eps   = 1e-8
-	alpha = [0.001 for i in range(len(parameters))]
-	if count == 0:
-		count = 1
-
-	new_shift = [0 for i in range(len(parameters))]
-
-	for i in range(len(parameters)):
-		m[i] = beta1 * m[i] + (1 - beta1) * g[i]
-		v[i] = beta2 * v[i] + (1 - beta2) * np.power(g[i],2)
-
-		alpha[i] = alpha[i] * np.sqrt(1 - np.power(beta2,count)) / (1 - np.power(beta1,count))
-
-		new_shift[i] = shift[i] + alpha[i]*(m[i]/(np.sqrt(v[i])+eps))
-
-	return new_shift
-
-
-def measure_overlap_pVQD(target,ansatz,num_qubits,trotter_steps,param_list):
-
-    ovp_list = []
-    
-    for params in param_list:
-        circ  = ansatz(num_qubits,trotter_steps,params)
-        t_wfn = CircuitStateFn(circ)
-        t_ovp = np.abs((~target@t_wfn).eval())**2
-
-        ovp_list.append(t_ovp)
-
-    return ovp_list
+    return new_params
 
 
--- final_submission/trotter_functions.py
+++ final_submission/trotter_functions.py
@@ -10,72 +10,118 @@ from qiskit                               import QuantumCircuit, QuantumRegister
 from qiskit.providers.aer                 import QasmSimulator
 from qiskit.circuit                       import Parameter
 
+ 
+## Functions for 2 qubits rotations
 
-# +
-## We start creating the functions for the circuit corresponding to useful 2-qubit gates
-# -
 
 def R_xx(t):
-	# This function returns the circuit for R_xx(t), as indicated in eq. 34 of arXiv:1907.03505v2
+    
+    '''
+    Circuit for R_xx(t), as indicated in eq. 34 of arXiv:1907.03505v2
+    
+    Args:
+        - t: parameter of the rotation
+        
+    Returns:
+        A QuantumCircuit implementing the R_xx(t) rotation with name XX
+    '''
 
-	XX_qr = QuantumRegister(2)
-	XX_qc = QuantumCircuit(XX_qr, name='XX')
-	
-	XX_qc.ry(np.pi/2,[0,1])
-	XX_qc.cnot(0,1)
-	XX_qc.rz(2 * t, 1)
-	XX_qc.cnot(0,1)
-	XX_qc.ry(-np.pi/2,[0,1])
+    XX_qr = QuantumRegister(2)
+    XX_qc = QuantumCircuit(XX_qr, name='XX')
 
-	return XX_qc
+    XX_qc.ry(np.pi/2,[0,1])
+    XX_qc.cnot(0,1)
+    XX_qc.rz(2 * t, 1)
+    XX_qc.cnot(0,1)
+    XX_qc.ry(-np.pi/2,[0,1])
+
+    return XX_qc
 
 def R_yy(t):
-    # This function returns the circuit for R_yy(t), as indicated in eq. 33 of arXiv:1907.03505v2
+    
+    '''
+    Circuit for R_yy(t), as indicated in eq. 33 of arXiv:1907.03505v2
+    
+    Args:
+        - t: parameter of the rotation
+        
+    Returns:
+        A QuantumCircuit implementing the R_xx(t) rotation with name YY
+    '''
 
-	YY_qr = QuantumRegister(2)
-	YY_qc = QuantumCircuit(YY_qr, name='YY')
-	
-	YY_qc.rx(np.pi/2,[0,1])
-	YY_qc.cnot(0,1)
-	YY_qc.rz(2 * t, 1)
-	YY_qc.cnot(0,1)
-	YY_qc.rx(-np.pi/2,[0,1])
+    YY_qr = QuantumRegister(2)
+    YY_qc = QuantumCircuit(YY_qr, name='YY')
+    
+    YY_qc.rx(np.pi/2,[0,1])
+    YY_qc.cnot(0,1)
+    YY_qc.rz(2 * t, 1)
+    YY_qc.cnot(0,1)
+    YY_qc.rx(-np.pi/2,[0,1])
 
-	return YY_qc
+    return YY_qc
 
 
 def R_zz(t):
-    # This function returns the circuit for R_zz(t), as indicated in eq. 32 of arXiv:1907.03505v2
+    
+    '''
+    Circuit for R_zz(t), as indicated in eq. 32 of arXiv:1907.03505v2
+    
+    Args:
+        - t: parameter of the rotation
+        
+    Returns:
+        A QuantumCircuit implementing the R_xx(t) rotation with name ZZ
+    '''
+    ZZ_qr = QuantumRegister(2)
+    ZZ_qc = QuantumCircuit(ZZ_qr, name='ZZ')
 
-	ZZ_qr = QuantumRegister(2)
-	ZZ_qc = QuantumCircuit(ZZ_qr, name='ZZ')
+    ZZ_qc.cnot(0,1)
+    ZZ_qc.rz(2 * t, 1)
+    ZZ_qc.cnot(0,1)
 
-	ZZ_qc.cnot(0,1)
-	ZZ_qc.rz(2 * t, 1)
-	ZZ_qc.cnot(0,1)
-
-	return ZZ_qc
+    return ZZ_qc
 
 def R_xyz(t):
-    # This function returns optimal gate decomposition of e^(-it(XX+YY+ZZ)), 
-    # as indicated in Fig. 4b of arXiv:1907.03505v2
-	XYZ_qr = QuantumRegister(2)
-	XYZ_qc = QuantumCircuit(XYZ_qr, name='XYZ')
-	
-	XYZ_qc.cnot(0,1)
-	XYZ_qc.rx(2*t-np.pi/2, 0)
-	XYZ_qc.rz(2 * t, 1)
-	XYZ_qc.h(0)
-	XYZ_qc.cnot(0,1)
-	XYZ_qc.h(0)
-	XYZ_qc.rz(-2 * t, 1)
-	XYZ_qc.cnot(0,1)
-	XYZ_qc.rx(np.pi/2,0)
-	XYZ_qc.rx(-np.pi/2,1)
+    
+    '''
+    Circuit implementing the optimal gate decomposition of e^(-it(XX+YY+ZZ))
+    as indicated in Fig. 4b of arXiv:1907.03505v2
+    
+    Args:
+        - t: parameter of the rotation
+        
+    Returns:
+        A QuantumCircuit implementing the R_{xx+yy+zz}(t) 3 CNOTs rotation with name XYZ
+    '''
+    XYZ_qr = QuantumRegister(2)
+    XYZ_qc = QuantumCircuit(XYZ_qr, name='XYZ')
+    
+    XYZ_qc.cnot(0,1)
+    XYZ_qc.rx(2*t-np.pi/2, 0)
+    XYZ_qc.rz(2 * t, 1)
+    XYZ_qc.h(0)
+    XYZ_qc.cnot(0,1)
+    XYZ_qc.h(0)
+    XYZ_qc.rz(-2 * t, 1)
+    XYZ_qc.cnot(0,1)
+    XYZ_qc.rx(np.pi/2,0)
+    XYZ_qc.rx(-np.pi/2,1)
 
-	return XYZ_qc
+    return XYZ_qc
 
 def R_xyz_var(p):
+    
+    '''
+    Circuit implementing the optimal gate decomposition of e^(-it(XX+YY+ZZ))
+    as indicated in Fig. 4b of arXiv:1907.03505v2,
+    now every gate depending on the rotation angle 't' is a parameterized gate
+    
+    Args:
+        - p: array of 3 parameters
+        
+    Returns:
+        A QuantumCircuit implementing the parameterized R_{xx+yy+zz}(p0,p1,p2) rotation with name XYZ-var
+    '''
     
     XYZ_qr = QuantumRegister(2)
     XYZ_qc = QuantumCircuit(XYZ_qr, name='XYZ-var')
@@ -99,104 +145,161 @@ def R_xyz_var(p):
 ## Now we create the Trotter circuit for the XXX Heisenberg model
 # -
 
-def Heisenberg_Trotter(num_qubits,trotter_steps,p,target_time):
+def Heisenberg_Trotter(num_qubits,trotter_steps,t,target_time):
+    
+    '''
+    Circuit implementing Trotterization of the time evolutiom operator for the XXX Heisenberg
+    model on num_qubits using the circuit indicated in Fig. 4a of arXiv:1907.03505v2.
+    Every  Trotter step requires 6*(num_qubits-1) CNOTs
+    
+    Args:
+        - num_qubits: int, number of qubits of the system
+        - trotter_steps: the number of trotter steps n to implement
+        - t: Qiskit Parameter object, will be binded to dt
+        - target_time: the simulation time we are targeting
+        
+    Returns:
+        A QuantumCircuit implementing the Trotterization of the time evolutiom operator for the XXX Heisenberg
+        model
+    '''
+    
+    # Given a target time and a number of Trotter steps, every step will evolve the 
+    # circuit for a time step dt = target_time/trotter_steps
+    dt = target_time/trotter_steps
 
-	dt = target_time/trotter_steps
+    XX = R_xx(t).to_instruction()
+    YY = R_yy(t).to_instruction()
+    ZZ = R_zz(t).to_instruction()
+    
+    
+    # Combine subcircuits into a single multiqubit gate representing a single trotter step
 
-	XX = R_xx(p).to_instruction()
-	YY = R_yy(p).to_instruction()
-	ZZ = R_zz(p).to_instruction()
-	
-	
-	# Combine subcircuits into a single multiqubit gate representing a single trotter step
+    
+    Trot_qr = QuantumRegister(num_qubits)
+    Trot_qc = QuantumCircuit(Trot_qr, name='Trot')
+    
+    for i in range(0, num_qubits - 1):
+        Trot_qc.append(ZZ, [Trot_qr[i], Trot_qr[i+1]])
+        Trot_qc.append(YY, [Trot_qr[i], Trot_qr[i+1]])
+        Trot_qc.append(XX, [Trot_qr[i], Trot_qr[i+1]])
 
-	
-	Trot_qr = QuantumRegister(num_qubits)
-	Trot_qc = QuantumCircuit(Trot_qr, name='Trot')
-	
-	for i in range(0, num_qubits - 1):
-		Trot_qc.append(ZZ, [Trot_qr[i], Trot_qr[i+1]])
-		Trot_qc.append(YY, [Trot_qr[i], Trot_qr[i+1]])
-		Trot_qc.append(XX, [Trot_qr[i], Trot_qr[i+1]])
-	
-	# Now repeat the circuit #trotter_reps
+    # Now repeat the circuit #trotter_reps
 
-	Trot_gate = Trot_qc.to_instruction()
-
-
-	# Initialize quantum circuit for 3 qubits
-	qr = QuantumRegister(num_qubits)
-	qc = QuantumCircuit(qr)
-
-
-	# Simulate time evolution under H_heis3 Hamiltonian
-	for _ in range(trotter_steps):
-		qc.append(Trot_gate, [qr[0], qr[1], qr[2]])
-		qc.barrier()
-
-	# Evaluate simulation at target_time meaning each trotter step evolves pi/trotter_steps in time
-	qc = qc.bind_parameters({p: dt})
-
-	return qc
-
-
-def Heisenberg_Trotter_compressed(num_qubits,trotter_steps,p,target_time):
-
-	dt = target_time/trotter_steps
-
-	XYZ = R_xyz(p).to_instruction()
-	
-	
-	# Combine subcircuits into a single multiqubit gate representing a single trotter step
-
-	
-	Trot_qr = QuantumRegister(num_qubits)
-	Trot_qc = QuantumCircuit(Trot_qr, name='Trot')
-	
-	for i in range(0, num_qubits - 1):
-		Trot_qc.append(XYZ, [Trot_qr[i], Trot_qr[i+1]])
-	
-	# Now repeat the circuit #trotter_reps
-
-	Trot_gate = Trot_qc.to_instruction()
-
-	# Initialize quantum circuit for 3 qubits
-	qr = QuantumRegister(num_qubits)
-	qc = QuantumCircuit(qr)
+    Trot_gate = Trot_qc.to_instruction()
 
 
-	# Simulate time evolution under H_heis3 Hamiltonian
-	for _ in range(trotter_steps):
-		qc.append(Trot_gate, [qr[0], qr[1], qr[2]])		
-		qc.barrier()
+    # Initialize quantum circuit for 3 qubits
+    qr = QuantumRegister(num_qubits)
+    qc = QuantumCircuit(qr)
 
-	# Evaluate simulation at target_time meaning each trotter step evolves pi/trotter_steps in time
-	qc = qc.bind_parameters({p: dt})
 
-	return qc
+    # Simulate time evolution under H_heis3 Hamiltonian
+    for _ in range(trotter_steps):
+        qc.append(Trot_gate, [qr[0], qr[1], qr[2]])
+        qc.barrier()
+
+    # Evaluate simulation at target_time meaning each trotter step evolves target_time/trotter_steps in time
+    qc = qc.bind_parameters({t: dt})
+
+    return qc
+
+
+def Heisenberg_Trotter_compressed(num_qubits,trotter_steps,t,target_time):
+    
+    '''
+    Circuit implementing Trotterization of the time evolutiom operator for the XXX Heisenberg
+    model on num_qubits using the circuit indicated in Fig. 4b of arXiv:1907.03505v2.
+    Every  Trotter step requires 3*(num_qubits-1) CNOTs
+    
+    Args:
+        - num_qubits: int, number of qubits of the system
+        - trotter_steps: the number of trotter steps n to implement
+        - t: Qiskit Parameter object, will be binded to dt
+        - target_time: the simulation time we are targeting
+        
+    Returns:
+        A QuantumCircuit implementing the Trotterization of the time evolutiom operator for the XXX Heisenberg
+        model
+    '''
+
+    # Given a target time and a number of Trotter steps, every step will evolve the 
+    # circuit for a time step dt = target_time/trotter_steps
+    dt = target_time/trotter_steps
+
+    XYZ = R_xyz(t).to_instruction()
+    
+    
+    # Combine subcircuits into a single multiqubit gate representing a single trotter step
+
+    
+    Trot_qr = QuantumRegister(num_qubits)
+    Trot_qc = QuantumCircuit(Trot_qr, name='Trot')
+    
+    for i in range(0, num_qubits - 1):
+        Trot_qc.append(XYZ, [Trot_qr[i], Trot_qr[i+1]])
+
+    # Now repeat the circuit #trotter_reps
+
+    Trot_gate = Trot_qc.to_instruction()
+
+    # Initialize quantum circuit for 3 qubits
+    qr = QuantumRegister(num_qubits)
+    qc = QuantumCircuit(qr)
+
+
+    # Simulate time evolution under H_heis3 Hamiltonian
+    for _ in range(trotter_steps):
+        qc.append(Trot_gate, [qr[0], qr[1], qr[2]])     
+        qc.barrier()
+
+    # Evaluate simulation at target_time meaning each trotter step evolves target_time/trotter_steps in time
+    qc = qc.bind_parameters({t: dt})
+
+    return qc
 
 
 def Heisenberg_Trotter_variational(num_qubits,trotter_steps,p):
 
-	qr = QuantumRegister(num_qubits)
-	qc = QuantumCircuit(qr)
-	count = 0
+    qr = QuantumRegister(num_qubits)
+    qc = QuantumCircuit(qr)
+    count = 0
 
-	qc.rx(np.pi,[1,2])
+    qc.rx(np.pi,[1,2])
 
-	for d in range(trotter_steps):
-		for i in range(num_qubits-1):
-			qc.append(R_xyz_var(p[count:count+3]).to_instruction(),[qr[i],qr[i+1]])
-			count += 3
+    for d in range(trotter_steps):
+        for i in range(num_qubits-1):
+            qc.append(R_xyz_var(p[count:count+3]).to_instruction(),[qr[i],qr[i+1]])
+            count += 3
 
-	return qc
+    return qc
 
 
-def Heisenberg_YBE_variational(num_qubits,p):
+def Heisenberg_YBE_variational(p):
 
-    circ = QuantumCircuit(num_qubits)
+    '''
+    Circuit implementing the YBE compression of the time evolutiom operator for the XXX Heisenberg model.
+    The YBE compression for Trotterized evolution was proposed in ArXiv:2112.01690.
+    The circuit is for 3 qubits and 4 compressed trotter step, for a total of 15 CNOTs and 15 parameters
+    
+    Args:
+        
+        - p: an array of 15 real parameters
+        
+    Returns:
+        A QuantumCircuit implementing the Trotterization of the time evolutiom operator for the XXX Heisenberg
+        model
+    '''
+
+    # Initialise the 3 qubit circuit
+    circ = QuantumCircuit(3)
     count = 0
     
+    # Circuit implementing the optimal gate decomposition of e^(-it(XX+YY+ZZ))
+    # as indicated in Fig. 4b of arXiv:1907.03505v2,
+    # now every gate depending on the rotation angle 't' is a parameterized gate.
+
+    # This means 3 parameters per e^(-it(XX+YY+ZZ)) decomposition
+
     def XYZ_variational(circ,i,j,params):
         circ.cx(i,j)
         circ.rx(params[0],i)
@@ -212,8 +315,8 @@ def Heisenberg_YBE_variational(num_qubits,p):
         circ.rx(np.pi/2,i)
         circ.rx(-np.pi/2,j)
 
-    circ.rx(np.pi,[1,2])
-    
+    # Apply the gate 5 times on qubits (1,2) and (0,1), alternatively.
+
     XYZ_variational(circ,1,2,p[count:count+3])
     count += 3
     XYZ_variational(circ,0,1,p[count:count+3])
